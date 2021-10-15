3dbd2c13492c6e17da2f5196d2fda78f25d678cb
Taeron <taeronpro@gmail.com>
2021-10-15 16:53:19 +0200

    add button listener

--- src/main/java/me/loic/bot/listeners/CommandListener.java
+++ src/main/java/me/loic/bot/listeners/CommandListener.java
@@ -1,6 +1,7 @@
 package me.loic.bot.listeners;
 
 import me.loic.bot.Bot;
+import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -13,5 +14,9 @@ public class CommandListener extends ListenerAdapter {
         Bot.getInstance().getCommandManager().getRegisteredCommands().stream()
                 .filter(command -> command.getName().equalsIgnoreCase(event.getName()))
                 .forEach(command -> command.execute(event));
+    }
+
+    @Override
+    public void onButtonClick(@Nonnull ButtonClickEvent event) {
     }
 }
